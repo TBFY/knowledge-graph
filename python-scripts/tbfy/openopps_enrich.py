890e1232ca99e551506234d719ea0efb750cf863
Brian Elvesæter <brian.elvesater@sintef.no>
2019-12-09 17:59:06 +0100

    Removed unnecessary tags in enrichment of releases.

--- python-scripts/tbfy/openopps_enrich.py
+++ python-scripts/tbfy/openopps_enrich.py
@@ -45,37 +45,11 @@ def enrich_release(release_data):
 
 
 def enrich_plan(release_data):
-    ocid = tbfy.json_utils.get_value(release_data, "releases.[0].ocid")
-    release_id = tbfy.json_utils.get_value(release_data, "releases.[0].id")
-
-    tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].planning", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].planning", "tbfy_release_id", release_id)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].planning.documents", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].planning.documents", "tbfy_release_id", release_id)
-
     enrich_tender(release_data)
-
     return release_data
 
 
 def enrich_tender(release_data):
-    ocid = tbfy.json_utils.get_value(release_data, "releases.[0].ocid")
-    tender_id = tbfy.json_utils.get_value(release_data, "releases.[0].tender.id")
-
-    tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].tender", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.items", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.items", "tbfy_tender_id", tender_id)
-    tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].tender.value", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].tender.value", "tbfy_tender_id", tender_id)
-    tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].tender.minValue", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].tender.minValue", "tbfy_tender_id", tender_id)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.documents", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.documents", "tbfy_tender_id", tender_id)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.milestones", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.milestones", "tbfy_tender_id", tender_id)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.amendment.changes", "tbfy_ocid", ocid)
-    tbfy.json_utils.add_property_to_array_node(release_data, "releases.[0].tender.amendment.changes", "tbfy_tender_id", tender_id)
-
     return release_data
 
 
@@ -88,16 +62,11 @@ def enrich_award(release_data):
         award_path = "releases.[0].awards.[" + str(i) + "]"
         award_id = tbfy.json_utils.get_value(release_data, award_path + ".id")
 
-        tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].awards.[" + str(i) + "].value", "tbfy_award_id", award_id)
-        tbfy.json_utils.add_property_to_single_node(release_data, "releases.[0].awards.[" + str(i) + "].contractPeriod", "tbfy_award_id", award_id)
-
         suppliers_data = release_data['releases'][0]['awards'][i]['suppliers']
         j = 0
         for supplier in suppliers_data:
             supplier_path = award_path + '.suppliers.[' + str(j) + ']'
-            tbfy.json_utils.add_property_to_single_node(release_data, supplier_path, "tbfy_ocid", ocid)
             tbfy.json_utils.add_property_to_single_node(release_data, supplier_path, "tbfy_id", j)
-            tbfy.json_utils.add_property_to_single_node(release_data, supplier_path, "tbfy_award_id", award_id)
             j += 1
 
         i += 1
