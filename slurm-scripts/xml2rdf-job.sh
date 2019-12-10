c25479a9ffe587c5cf99e91def6e91c2fec29a77
Brian Elvesæter <brian.elvesater@sintef.no>
2019-12-10 11:43:24 +0100

    Updated Slurm script for running xml2rdf jobs.

---
+++ slurm-scripts/xml2rdf-job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --output /home/bre/jobs/xml2rdf-%j.out
+#SBATCH --job-name XML2RDF
+#SBATCH --partition sintef
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=2GB
+#SBATCH --time 7-00:00:00
+
+homedir=/home/bre/knowledge-graph/python-scripts
+
+cd $homedir
+export DATE=`date +%F_%H%M`
+srun python -u xml2rdf.py -s '2019-01-01' -e '2019-01-31' -r '/data/bre/RML_Mapper_Scripts' -i '/data/bre/4_XML_Enriched' -o '/data/bre/5_RDF_TBFY' > /home/bre/jobs/job_$DATE.log 2>&1
