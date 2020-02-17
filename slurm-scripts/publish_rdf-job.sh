6f495e3a7f8887f09795dbd9f4790d27910b65bd
Brian Elvesæter <brian.elvesater@sintef.no>
2020-02-17 12:26:30 +0100

    Added new SLURM script for publishing RDF.

---
+++ slurm-scripts/publish_rdf-job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --output /home/bre/jobs/publish_rdf-%j.out
+#SBATCH --job-name XML2RDF
+#SBATCH --partition sintef
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8GB
+#SBATCH --time 7-00:00:00
+
+homedir=/home/bre/knowledge-graph/python-scripts
+
+cd $homedir
+export DATE=`date +%F_%H%M`
+srun python -u publish_rdf.py -s '2019-01-01' -e '2019-01-31' -i '/data/bre/5_RDF_TBFY' > /home/bre/jobs/job_$DATE.log 2>&1
