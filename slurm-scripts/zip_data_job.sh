e8b0d5b39fd0d78c4a06daf43baafa7710a47434
Brian Elvesæter <brian.elvesater@sintef.no>
2020-06-02 13:52:25 +0200

    Added Slurm script for creating the JSON and RDF data dumps.

---
+++ slurm-scripts/zip_data_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --output /home/bre/jobs/zip_data-%j.out
+#SBATCH --job-name ZIP_DATA
+#SBATCH --partition sintef
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8GB
+#SBATCH --time 1-00:00:00
+
+homedir=/data/bre
+
+cd $homedir
+export DATE=`date +%F_%H%M`
+srun zip -r 1_JSON_OpenOpps.zip 1_JSON_OpenOpps && srun zip -r 2_JSON_OpenCorporates.zip 2_JSON_OpenCorporates && srun zip -r 5_RDF_TBFY.zip 5_RDF_TBFY > /home/bre/jobs/job_$DATE.log 2>&1
