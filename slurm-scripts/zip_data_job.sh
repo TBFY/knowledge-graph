6e72f877bcf44ae664744a67d6525b9a5082490e
Brian Elvesæter <brian.elvesater@sintef.no>
2021-02-09 22:46:16 +0100

    Converted format of script file from DOS to UNIX using dos2unix.

--- slurm-scripts/zip_data_job.sh
+++ slurm-scripts/zip_data_job.sh
@@ -12,3 +12,4 @@ homedir=/data/bre
 cd $homedir
 export DATE=`date +%F_%H%M`
 srun zip -r 1_JSON_OpenOpps.zip 1_JSON_OpenOpps && srun zip -r 2_JSON_OpenCorporates.zip 2_JSON_OpenCorporates && srun zip -r 3_JSON_Enriched.zip 3_JSON_Enriched && srun zip -r 4_XML_Enriched.zip 4_XML_Enriched && srun zip -r 5_RDF_TBFY.zip 5_RDF_TBFY > /home/bre/jobs/job_$DATE.log 2>&1
+
